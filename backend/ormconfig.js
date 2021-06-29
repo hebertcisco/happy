741f07e27507665e19bcb2dd2c14383cc4eec030
Restyled.io <commits@restyled.io>
2021-06-29 23:37:33 +0000

    Restyled by clang-format

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,24 +1,18 @@
 module.exports = {
-  "type": "postgres",
-  "host": process.env.TYPEORM_HOST,
-  "port":  process.env.TYPEORM_PORT,
-  "username":  process.env.TYPEORM_USERNAME,
-  "password":  process.env.TYPEORM_PASSWORD,
-  "database":  process.env.TYPEORM_DATABASE,
-  "synchronize": true,
-  "logging": false,
-  "entities": [
-    "src/models/*.ts"
-  ],
-  "migrations": [
-    "src/database/migration/*.ts"
-  ],
-  "subscribers": [
-    "src/subscriber/**/*.ts"
-  ],
-  "cli": {
-    "entitiesDir": "src/models",
-    "migrationsDir": "src/database/migrations",
-    "subscribersDir": "src/subscriber"
+  "type" : "postgres",
+  "host" : process.env.TYPEORM_HOST,
+  "port" : process.env.TYPEORM_PORT,
+  "username" : process.env.TYPEORM_USERNAME,
+  "password" : process.env.TYPEORM_PASSWORD,
+  "database" : process.env.TYPEORM_DATABASE,
+  "synchronize" : true,
+  "logging" : false,
+  "entities" : [ "src/models/*.ts" ],
+  "migrations" : [ "src/database/migration/*.ts" ],
+  "subscribers" : [ "src/subscriber/**/*.ts" ],
+  "cli" : {
+    "entitiesDir" : "src/models",
+    "migrationsDir" : "src/database/migrations",
+    "subscribersDir" : "src/subscriber"
   }
 }
