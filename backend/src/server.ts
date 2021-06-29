c5a3c0e4f366810e19d92d23aec1a625634010a5
hebertcisco <tecnobertinfo@gmail.com>
2021-06-29 20:35:24 -0300

    Add postgres support
    Added postgres support and basic config to run this with env vars

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,8 +7,12 @@ import './database/connection'
 
 import routes from './routes'
 import errorHandler from './errors/handler'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+const port  = process.env.PORT
 
 app.use(cors())
 app.use(express.json())
@@ -16,5 +20,4 @@ app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(errorHandler)
 
-
-app.listen(3333)
+app.listen(Number(port) || 3333)
